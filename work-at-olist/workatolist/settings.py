9ea751177b73b5bab2ce517f8a78600dc0e8c90d
Guilherm Tavares <zetavares.rib@gmail.com>
2017-01-11 21:36:12 -0200

    docs, procfile

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_swagger',
 
     'channels.apps.ChannelsConfig',
 ]
--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -2,16 +2,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from channels.views import ChannelListAPIView
 
-# router = routers.DefaultRouter()
-# router.register(r'channel', ChannelListAPIView)
-# # router.register(r'category', CategoryView)
-
+schema_view = get_swagger_view(title='Work AT Olist API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include('channels.urls')),
+
+    url(r'^$', schema_view),
 ]
