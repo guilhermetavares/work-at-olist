3351de3fde54a92656170681d9ff798a59d62ff4
Guilherm Tavares <zetavares.rib@gmail.com>
2017-01-11 00:18:59 -0200

    add APIClient in ChannelViewsTestCase

--- work-at-olist/channels/tests/test_views.py
+++ work-at-olist/channels/tests/test_views.py
@@ -1,12 +1,22 @@
-from django.test import TestCase, RequestFactory
+import json
+
+from django.test import TestCase
+
+from rest_framework.test import APIClient
+
+from .factories import ChannelFactory
 
 
 class ChannelViewsTestCase(TestCase):
 
 	def setUp(self):
-		self.factory = RequestFactory()
+		self.client = APIClient()
 
 	def test_01_channels_list(self):
-		request = self.factory.get('/api/v1/channels/')
-		print(request)
-		print(dir(request))
+		response = self.client.get('/api/v1/channels/')
+		assert response.status_code == 200
+
+	def test_01_channels_list_contents(self):
+		channel = ChannelFactory.create()
+		response = self.client.get('/api/v1/channels/')
+		print(json.loads(response.content.decode()))
