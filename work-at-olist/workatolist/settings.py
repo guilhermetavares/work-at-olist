daf343ed318d09478a4502f65a675333fca49976
Guilherm Tavares <zetavares.rib@gmail.com>
2017-01-11 22:16:36 -0200

    settings for static, add static folder

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -128,9 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
